068418f31213724ae823c98fdc96a10274870b32
Bruno Peres <bruno_16515@hotmail.com>
2022-08-09 08:56:34 -0300

    Buscando um único documento

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const { ObjectId } = require("mongodb");
+
 const { connectToDb, getDb } = require("./db");
 
 // Initialize App & Middleware
@@ -16,6 +18,8 @@ connectToDb(err => {
 });
 
 // Routes
+
+// Buscar todos os livros
 app.get("/books", (req, res) => {
   let books = [];
 
@@ -31,4 +35,21 @@ app.get("/books", (req, res) => {
     .catch(() => {
       res.status(500).json({ error: "Couldn't find books." });
     });
+});
+
+// Buscar um único livro
+app.get("/books/:id", (req, res) => {
+  // Verifica se o parâmetro passado é válido
+  if (ObjectId.isValid(req.params.id)) {
+    db.collection("books")
+      .findOne({ _id: ObjectId(req.params.id) })
+      .then(doc => {
+        res.status(200).json(doc);
+      })
+      .catch(() => {
+        res.status(500).json({ error: "Couldn't find the book." });
+      });
+  } else {
+    res.status(500).json({ error: "Not a valid book id." });
+  }
 });
--- db.js
+++ db.js
@@ -2,18 +2,16 @@ const { MongoClient } = require("mongodb");
 
 let dbConnection;
 
-module.exports = {
-  connectToDb: cb => {
-    MongoClient.connect("mongodb://localhost:27017/bookstore")
-      .then(client => {
-        dbConnection = client.db();
-        return cb();
-      })
-      .catch(err => {
-        console.log(err);
-        return cb(err);
-      });
-  },
+function connectToDb(cb) {
+  MongoClient.connect("mongodb://localhost:27017/bookstore")
+    .then(client => {
+      dbConnection = client.db();
+      return cb();
+    })
+    .catch(err => {
+      console.log(err);
+      return cb(err);
+    });
+}
 
-  getDb: () => dbConnection,
-};
+module.exports = { connectToDb, getDb: () => dbConnection };
